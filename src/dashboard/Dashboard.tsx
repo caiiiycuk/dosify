a40dff73e3e87fea1ae991d0436fba0748ff17b0
Aleksander Guryanov <caiiiycuk@gmail.com>
2019-06-02 21:30:46 +0700

    Just a react app template

---
+++ src/dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import { H2 } from "@blueprintjs/core";
+
+import React from "react";
+
+const Dashboard: React.FC = () => {
+    return <div>
+        <FileInput disabled={true} text="Choose file..." onInputChange={...} />
+    </div>;
+};
+
+export default Dashboard;
