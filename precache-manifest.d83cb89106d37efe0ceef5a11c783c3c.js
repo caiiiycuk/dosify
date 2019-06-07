45b94e115dbb0775c04cc09ce5b15e3ec314f4e0
Aleksander Guryanov <caiiiycuk@gmail.com>
2019-06-07 22:31:13 +0700

    Updates

---
+++ precache-manifest.d83cb89106d37efe0ceef5a11c783c3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58231858fe69f9ac6781efc8554e0334",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a82f4346c1325e2d2fe",
+    "url": "/static/css/2.3a030ef9.chunk.css"
+  },
+  {
+    "revision": "9d6e85b2af435725f9cb",
+    "url": "/static/css/main.fd483c59.chunk.css"
+  },
+  {
+    "revision": "8a82f4346c1325e2d2fe",
+    "url": "/static/js/2.8cec6f6c.chunk.js"
+  },
+  {
+    "revision": "9d6e85b2af435725f9cb",
+    "url": "/static/js/main.27cbb34f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
